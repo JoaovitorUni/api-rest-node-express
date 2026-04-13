3cecbfd990107356914a44654cf0b8e1bdcb3a04
João Vitor <joaovitordsimao@edu.unifil.br>
2026-04-13 09:32:56 -0300

    refactor: coloca port como env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ loadEnvFile();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 migrate(db);
 populate(db);
